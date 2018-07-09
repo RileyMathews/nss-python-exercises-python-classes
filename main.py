37dad899642fb8712f7d5e35d71d176e26436ca4
Riley Mathews <rileymathews80@gmail.com>
2018-07-09 14:11:46 -0500

    modified classes per joes example code

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from dog import Dog
 from pet import Pet
 
-buddy = Pet("buddy", Dog("Dachshound"))
+dacshound = Dog("Dacshound")
+
+buddy = Pet("buddy", dacshound)
+
+buddy.set_owner("Riley", "555-555-5432")
 
 print(buddy)
--- pet.py
+++ pet.py
@@ -1,12 +1,13 @@
 
 class Pet():
 
-    def __init__(self, name, animal_type):
+    def __init__(self, name, critter_instance):
         self.name = name
-        self.animal_type = animal_type
+        self.animal_type = critter_instance
 
-    def assign_owner(self, owner):
-        self.owner = owner
+    def set_owner(self, name, phone):
+        self.owner_name = name
+        self.owner_number = phone
 
     def __str__(self):
         return f'My name is {self.name}, I have {self.animal_type.leg_num} legs, and I say {self.animal_type.speak()}'
